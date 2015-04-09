76cf87305c8f7811b402d8a2b4b8840c7f1ddfce
Anamor7 <tburkhart@unomaha.edu>
2015-04-09 15:16:09 -0500

    Added SQL query parsing

--- src/buffermanager/BufferManager.java
+++ src/buffermanager/BufferManager.java
@@ -2,7 +2,10 @@ package buffermanager;
 
 import java.io.File;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 import java.io.FileWriter;
+
+import com.sun.xml.internal.ws.util.StringUtils;
 
 /**
  *
@@ -92,5 +95,80 @@ public class BufferManager {
         for (Record item : edList){
             System.out.print(item);
         }
+        
+        String query1 = "select 1select, 2 from 3 NATURAL JOIN 4 where 5 = 6 or 7 = 8;";
+        String[] selectStatement = querySelect(query1);
+        String[] fromStatement = queryFrom(query1);
+        String[] whereStatement = queryWhere(query1);
+        System.out.print("Array = ");
+        for (int i = 0; i < whereStatement.length; i++){
+        	System.out.println(i + " = " + whereStatement[i]);
+        }
+        System.out.println();
+        //System.out.println("Size of statement is: " + selectStatement.length);
+        System.out.println("query = " + query1);
     }
+    
+    // returns select statement
+    public static String[] querySelect(String theQuery){
+    	
+    	theQuery = theQuery.toLowerCase();
+    	
+    	// Get string from beginning up until From or ;
+    	int i = theQuery.indexOf("from");
+    	if (i == -1 || theQuery.indexOf(';') < i){
+    		i = theQuery.indexOf(';');
+    	}
+    	theQuery = theQuery.substring(0,i);
+    	
+    	//System.out.println("theQuery = " + theQuery);
+    	
+    	String delims = "[ ,;]\\s*";
+        String[] tokens = theQuery.split(delims);
+        
+    	return tokens;
+    }
+    
+    // returns from
+    public static String[] queryFrom(String theQuery){
+    	theQuery = theQuery.toLowerCase();
+    	
+    	// Get string from 'from' up until where or ;
+    	int i = theQuery.indexOf("where");
+    	if (i == -1 || theQuery.indexOf(';') < i){
+    		i = theQuery.indexOf(';');
+    	}
+    	theQuery = theQuery.substring(theQuery.indexOf("from"),i);
+    	
+    	//System.out.println("theQuery = " + theQuery);
+    	
+    	String delims = "[ ;]\\s*";
+        String[] tokens = theQuery.split(delims);
+        
+    	return tokens;
+    }
+    
+    // returns where
+    public static String[] queryWhere(String theQuery){
+    	theQuery = theQuery.toLowerCase();
+    	
+    	// Get string from 'from' up until where or ;
+    	int i = theQuery.indexOf(';');
+    	theQuery = theQuery.substring(theQuery.indexOf("where"),i);
+    	
+    	//System.out.println("theQuery = " + theQuery);
+    	
+    	String delims = "[ ;]\\s*";
+        String[] tokens = theQuery.split(delims);
+        
+    	return tokens;
+    }
+    
+    /*
+    public static String[] removeEmpty(String[] queryArray){
+    	String query = "";
+    	query += queryArray;
+    	
+    }
+    */
 }
