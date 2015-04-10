1f5977c40734904c5115ad8a260b123d424b5293
Tyler Woodside <twoodside@unomaha.edu>
2015-04-09 19:46:04 -0500

    I'm going home because I'm very hungry, but I screwed up and didn't make another work branch so now we're going to have errors. Awesome.

---
+++ src/buffermanager/JoinedRecord.java
@@ -0,0 +1,13 @@
+package buffermanager;
+
+public class JoinedRecord extends Record implements Comparable<JoinedRecord>{
+	public static final String fname="";
+	
+	Record rec1;
+	Record rec2;
+	
+	public int compareTo(JoinedRecord e){
+		return 0;
+	}
+
+}
