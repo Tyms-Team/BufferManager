17fd3e7b552f56ba427583fa8644f6fd05e9b5ed
twoodside <twoodside@unomaha.edu>
2015-04-12 01:59:39 -0500

    Basic SQL stuff done. Need to add joins and * operator in select. Also need to figure out what the buffer is actually supposed to do, hopefully the teacher emails me back.

--- src/buffermanager/BufferManager.java
+++ src/buffermanager/BufferManager.java
@@ -21,28 +21,26 @@ public class BufferManager {
     private static int bufferCount=0;
     private static int bufferSize=0;
     
-    /*
-    private static Employee[] empList = new Employee[11];
-    private static Department[] deptList = new Department[12];
-    private static EmployeeDepartment[] edList = new EmployeeDepartment[13];
-    */
-    
     public static void main(String[] args) {
     	Scanner in=init(args);
     	String query = in.nextLine();
     	
     	String[] sList = queryFrom(query);
-    	
-    	Scanner objectIn=openFile(sList);
+        
+        Scanner objectIn=openFile(sList);
     	
     	sList = queryWhere(query);
     	
+        String[] projectionList = querySelect(query);
+        
+        printBoxHeader(projectionList);
     	while (objectIn.hasNext()){
-    		String tuple=objectIn.nextLine();
-    		if (passesCheck(tuple,sList))
-    			//System.out.println(tuple);
-    			;
+            String tuple=objectIn.nextLine();
+            if (passesCheck(tuple,sList)){
+                printBoxLine(projectionList,tuple);
+            }
     	}
+        printBoxFooter(projectionList);
     }
     
     /**
@@ -187,8 +185,79 @@ public class BufferManager {
     }
     
     public static boolean passesCheck(String tuple,String[] checkRules){
-    	compileCheck(checkRules); 
-    	return true;
+        boolean r=false;
+        String lvalue=null;
+        String op=null;
+        String boolOp=null;
+        
+        for (String s: checkRules){
+            if (s.equals("")){
+                continue;
+            }
+            else if (s.matches("^(?:\\|\\||&&)$")){
+                boolOp=s;
+            }
+            else if (s.matches("^(?:[><]?=|[><])$")){
+                op=s;
+            }
+            //else if (s.matches("^(?:employee|department)\\.id$|^employeedepartment\\.employee_id$")){
+            else if (s.matches("^id$|^employee_id$")){
+                lvalue=tuple.substring(0,tuple.indexOf(','));
+            }
+            else if (s.matches("^name$|^dept_id$")){
+                lvalue=tuple.substring(tuple.indexOf(',')+1);
+            }
+            else {
+                int compvalue;
+                //Figure out the comparison value
+                if (s.matches("^\".*\"$")){
+                    s=s.substring(1,s.length()-1);
+                    compvalue = lvalue.compareTo(s);
+                }
+                else if ( s.matches("\\d+")){
+                    Integer i=Integer.parseInt(lvalue);
+                    Integer j=Integer.parseInt(s);
+                    
+                    compvalue=i.compareTo(j);
+                }
+                else{
+                    return false;
+                }
+                
+                //Figure out if the comparison value is part of the operator
+                boolean sr;
+                if (compvalue<0 && op.matches("<")){
+                    sr=true;
+                }
+                else if (compvalue==0 && op.equals("=")){
+                    sr=true;
+                }
+                else if (compvalue>0 && op.matches(">")){
+                    sr=true;
+                }
+                else{
+                    sr=false;
+                }
+                
+                
+                
+                //Apply any boolean operators
+                if (boolOp==null){
+                    r=sr;
+                }
+                else if (boolOp.equals("&&")){
+                    r=r && sr;
+                    boolOp=null;
+                }
+                else if (boolOp.equals("||")){
+                    r=r || sr;
+                    boolOp=null;
+                }
+            }
+        }
+        if (boolOp!=null)
+            return false;
+    	return r;
     }
     
     public static String[] compileCheck(String[] checkRules){
@@ -211,5 +280,98 @@ public class BufferManager {
     		}
     	}
     	return r;
+    }
+
+    private static void printBoxHeader(String[] projectionList) {
+        boolean printfinal=true;
+        System.out.print('+');
+        
+        for (String s: projectionList){
+            if (!s.equals("")){
+                int end=(15-s.length())>>1;
+                for (int i=0;i<end;i++){
+                    System.out.print('-');
+                }
+                System.out.print(s);
+                
+                end=end-1*((s.length()%2==1)?1:0);
+                for (int i=0;i<end;i++){
+                    System.out.print('-');
+                }
+                printfinal=false;
+                System.out.print('+');
+            }
+        }
+        if (printfinal)
+            System.out.print('+');
+        System.out.println();
+    }
+
+    private static void printBoxFooter(String[] projectionList) {
+        boolean printfinal=true;
+        System.out.print('+');
+        
+        for (String s: projectionList){
+            if (!s.equals("")){
+                for (int i=0;i<14;i++){
+                    System.out.print('-');
+                }
+                System.out.print('+');
+                printfinal=false;
+            }
+        }
+        if (printfinal)
+            System.out.print('+');
+        System.out.println();
+    }
+
+    private static void printBoxLine(String[] projectionList, String tuple) {
+        boolean printfinal=true;
+        System.out.print('|');
+        
+        for (String s: projectionList){
+            int end;
+            switch (s){
+                case "":
+                    break;
+                case "id":
+                case "employee_id":
+                    s=tuple.substring(0,tuple.indexOf(','));
+                    end=(15-s.length())>>1;
+                    for (int i=0;i<end;i++){
+                        System.out.print(' ');
+                    }
+                    System.out.print(s);
+                    
+                    end=end-1*((s.length()%2==1)?1:0);
+                    for (int i=0;i<end;i++){
+                        System.out.print(' ');
+                    }
+                    System.out.print('|');
+                    printfinal=false;
+                    break;
+                case "name":
+                case "dept_id":
+                    s=tuple.substring(tuple.indexOf(',')+1);
+                    end = (15-s.length())>>1;
+                    for (int i=0;i<end;i++){
+                        System.out.print(' ');
+                    }
+                    System.out.print(s);
+                    
+                    end=end-1*((s.length()%2==1)?1:0);
+                    for (int i=0;i<end;i++){
+                        System.out.print(' ');
+                    }
+                    System.out.print('|');
+                    printfinal=false;
+                    break;
+            }
+        }
+        
+        
+        if (printfinal)
+            System.out.print('|');
+        System.out.println();
     }
 }
